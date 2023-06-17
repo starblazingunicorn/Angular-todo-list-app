ec7a14ba7788d7101c6dbf62888a6e37039ba299
Viyan.UX Engineer <74237057+starblazingunicorn@users.noreply.github.com>
2023-06-17 15:18:24 +0200

    add ui elment

--- todo/src/app/task-list/task-list.component.ts
+++ todo/src/app/task-list/task-list.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'task-list',
   templateUrl: './task-list.component.html',
-  styleUrls: ['./task-list.component.scss']
+  styleUrls: ['./task-list.component.scss'],
 })
 export class TaskListComponent {
-
+  text: string = '';
+  changeText(event: Event) {
+    const target = event.target as HTMLInputElement;
+    console.log(target.value);
+  }
+  addToDo():void{
+    console.log(this.text);
+  }
 }
