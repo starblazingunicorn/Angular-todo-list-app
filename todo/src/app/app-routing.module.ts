efb4155dac212296d370b87b8ea01aeb0477e613
Viyan.UX Engineer <74237057+starblazingunicorn@users.noreply.github.com>
2023-06-17 11:21:02 +0200

    added router and 404 page

--- todo/src/app/app-routing.module.ts
+++ todo/src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { TaskListComponent } from './task-list/task-list.component';
+import { AppComponent } from './app.component';
+import { TaskItemComponent } from './task-item/task-item.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'app',
+    pathMatch: 'full',
+  },
+  {
+    path: 'app',
+    component: AppComponent,
+  },
+  {
+    path: 'item',
+    component: TaskItemComponent,
+  },
+  {
+    path: 'list',
+    component: TaskListComponent,
+  },
+  { path: '404', component: NotfoundComponent },
+  { path: '**', redirectTo: '/404' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- todo/src/app/app.module.ts
+++ todo/src/app/app.module.ts
@@ -1,22 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TaskListComponent } from './task-list/task-list.component';
 import { TaskItemComponent } from './task-item/task-item.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TaskListComponent,
-    TaskItemComponent
+    TaskItemComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
